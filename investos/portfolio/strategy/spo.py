2daae67cce14cf0d425acefcfc8f40f944355004
Samuel Dion-Girardeau <samuel.diongirardeau@gmail.com>
2023-07-14 20:53:55 -0400

    Remove * imports from spo.py
    Makes it harder to read, more error-prone, and harder for static analysis tools

--- investos/portfolio/strategy/spo.py
+++ investos/portfolio/strategy/spo.py
@@ -2,8 +2,14 @@ import pandas as pd
 import datetime as dt
 import cvxpy as cvx
 
-from investos.portfolio.constraint_model import *
-from investos.portfolio.risk_model import *
+from investos.portfolio.constraint_model import (
+    BaseConstraint,
+    EqualLongShortConstraint,
+    MaxLeverageConstraint,
+    MaxWeightConstraint,
+    MinWeightConstraint,
+)
+from investos.portfolio.risk_model import BaseRisk, StatFactorRisk
 from investos.portfolio.strategy import BaseStrategy
 from investos.portfolio.cost_model import TradingCost, HoldingCost, BaseCost
 from investos.util import values_in_time
