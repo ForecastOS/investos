813512d677386427e68dcb98a8cd72f1cc36ef35
John Reese <j.charles.reese@gmail.com>
2023-11-05 23:52:45 -0800

    Pushing new version of InvestOS

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@ import sys
 project = "InvestOS"
 copyright = "2023, InvestOS"
 author = "Charlie Reese, ForecastOS"
-release = "0.3.4"
+release = "0.3.5"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
--- investos/__init__.py
+++ investos/__init__.py
@@ -1,6 +1,6 @@
 import investos.portfolio
 
-__version__ = "0.3.4"
+__version__ = "0.3.5"
 
 import os
 
