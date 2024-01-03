736b2ada7533e210f46341984425ea0f4080744c
John Reese <j.charles.reese@gmail.com>
2024-01-02 20:10:53 -0800

    Updating the simple t-cost model to more closely approximate (by default) live trades at AQR documented in Trading Costs (April 7, 2018) paper

--- investos/portfolio/cost_model/trading_cost.py
+++ investos/portfolio/cost_model/trading_cost.py
@@ -11,10 +11,18 @@ from investos.util import remove_excluded_columns_pd, values_in_time
 class TradingCost(BaseCost):
     """Calculates per period cost for trades `u`, based on spread, standard deviation, volume, and price.
 
+    Actual t-cost calculation based on AQR's research
+    on market impact for live trades from their execution database
+    between 1998 and 2016 (Figure 5):
+
+    Frazzini, Andrea and Israel, Ronen and Moskowitz, Tobias J. and Moskowitz, Tobias J.,
+    Trading Costs (April 7, 2018).
+    Available at SSRN: https://ssrn.com/abstract=3229719
+
     Attributes
     ----------
     sensitivity_coeff : float
-        For scaling transaction cost; 1 assumes 1 period's volume moves price by 1 std_dev in price
+        For scaling volume-based transaction cost component. Does not impact spread related costs.
     """
 
     def __init__(self, forecast_volume, actual_prices, **kwargs):
@@ -29,22 +37,25 @@ class TradingCost(BaseCost):
             exclude_assets=self.exclude_assets,
             include_assets=self.include_assets,
         )
-        # For scaling realized transaction cost; 1 assumes trading 1 day's volume moves price by 1 std dev
-        # Look for better research on this and default value
-        self.sensitivity_coeff = remove_excluded_columns_pd(
-            kwargs.get("price_movement_sensitivity", 1),
-            exclude_assets=self.exclude_assets,
-            include_assets=self.include_assets,
-        )
-        self.forecast_std_dev = remove_excluded_columns_pd(
-            kwargs.get("forecast_std_dev", 0.015),
-            exclude_assets=self.exclude_assets,
-            include_assets=self.include_assets,
+        self.sensitivity_coeff = (
+            remove_excluded_columns_pd(
+                kwargs.get("price_movement_sensitivity", 1),
+                exclude_assets=self.exclude_assets,
+                include_assets=self.include_assets,
+            )
+            / 100  # Convert to bps
         )
         self.half_spread = remove_excluded_columns_pd(
-            kwargs.get("half_spread", 5 / 10_000),
+            kwargs.get("half_spread", 1 / 10_000),
             exclude_assets=self.exclude_assets,
             include_assets=self.include_assets,
+        )
+        self.est_opt_cost_config = kwargs.get(
+            "est_opt_cost_config",
+            {
+                "linear_mi_multiplier": 2,
+                "min_half_spread": 8 / 10_000,
+            },
         )
 
     def _estimated_cost_for_optimization(self, t, w_plus, z, value):
@@ -54,33 +65,51 @@ class TradingCost(BaseCost):
         """
         constraints = []
 
-        std_dev = values_in_time(self.forecast_std_dev, t)
         volume_dollars = values_in_time(self.forecast_volume, t) * values_in_time(
             self.actual_prices, t
         )
-        percent_volume_traded_pre_trade_weight = (
-            np.abs(value) / volume_dollars
-        )  # Multiplied (using cvx) by trade weight (z) below!
+        percent_volume_traded_pre_trade_weight = np.abs(value) / volume_dollars
 
-        price_movement_term = (
-            self.sensitivity_coeff * std_dev * percent_volume_traded_pre_trade_weight
-        )
-
-        try:  # Spread (estimated) costs
+        try:  # Spread (convex, estimated) costs
             self.estimate_expression = cvx.multiply(
-                values_in_time(self.half_spread, t), cvx.abs(z)
+                np.clip(
+                    values_in_time(self.half_spread, t),
+                    self.est_opt_cost_config["min_half_spread"],
+                    None,
+                ),
+                cvx.abs(z),
             )
         except TypeError:
             self.estimate_expression = cvx.multiply(
-                values_in_time(self.half_spread, t).values,
+                np.clip(
+                    values_in_time(self.half_spread, t).values,
+                    self.est_opt_cost_config["min_half_spread"],
+                    None,
+                ),
                 cvx.abs(z),
             )
 
-        try:  # Price movement due to volume (estimated) costs
-            self.estimate_expression += cvx.multiply(price_movement_term, cvx.abs(z))
+        try:  # Market impact (convex, estimated) costs
+            self.estimate_expression += (
+                cvx.multiply(
+                    cvx.multiply(
+                        percent_volume_traded_pre_trade_weight,
+                        cvx.abs(z),
+                    ),
+                    self.sensitivity_coeff,
+                )
+                * self.est_opt_cost_config["linear_mi_multiplier"]
+            )
         except TypeError:
-            self.estimate_expression += cvx.multiply(
-                price_movement_term.values, cvx.abs(z)
+            self.estimate_expression += (
+                cvx.multiply(
+                    cvx.multiply(
+                        percent_volume_traded_pre_trade_weight.values,
+                        cvx.abs(z),
+                    ),
+                    self.sensitivity_coeff,
+                )
+                * self.est_opt_cost_config["linear_mi_multiplier"]
             )
 
         return cvx.sum(self.estimate_expression), constraints
@@ -89,14 +118,13 @@ class TradingCost(BaseCost):
         self, t: dt.datetime, h_plus: pd.Series, u: pd.Series
     ) -> pd.Series:
         spread_cost = np.abs(u) * values_in_time(self.half_spread, t)
-        std_dev = values_in_time(self.forecast_std_dev, t)
         volume_dollars = values_in_time(self.forecast_volume, t) * values_in_time(
             self.actual_prices, t
         )
         percent_volume_traded = np.abs(u) / volume_dollars
 
         trading_costs = spread_cost + (
-            self.sensitivity_coeff * std_dev * np.abs(u) * percent_volume_traded
+            self.sensitivity_coeff * np.abs(u) * (percent_volume_traded**0.5)
         )
 
         return trading_costs.sum()
