240ebb848b40b6f49decdd3613a9374f9a3d6889
Charlie Reese <j.charles.reese@gmail.com>
2024-08-04 14:14:40 -0700

    V0.5.1 (#61)
    * Adding adjustment to loading matrix columns
    
    * Pushing new version of InvestOS

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@ import sys
 project = "InvestOS"
 copyright = "2024, InvestOS"
 author = "Charlie Reese, ForecastOS"
-release = "0.5.0"
+release = "0.5.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
--- investos/__init__.py
+++ investos/__init__.py
@@ -1,6 +1,6 @@
 import investos.portfolio
 
-__version__ = "0.5.0"
+__version__ = "0.5.1"
 
 import os
 
