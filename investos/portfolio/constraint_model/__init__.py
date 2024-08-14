5a5db4ca7f5fd2c0000f16edbd64ab49cefeae3b
Charlie Reese <j.charles.reese@gmail.com>
2024-08-13 21:45:44 -0700

    Pushing new version of InvestOS (#62)

--- investos/portfolio/constraint_model/__init__.py
+++ investos/portfolio/constraint_model/__init__.py
@@ -2,5 +2,6 @@ from investos.portfolio.constraint_model.base_constraint import *
 from investos.portfolio.constraint_model.factor_constraint import *
 from investos.portfolio.constraint_model.leverage_constraint import *
 from investos.portfolio.constraint_model.long_constraint import *
+from investos.portfolio.constraint_model.return_constraint import *
 from investos.portfolio.constraint_model.trade_constraint import *
 from investos.portfolio.constraint_model.weight_constraint import *
---
+++ investos/portfolio/constraint_model/return_constraint.py
@@ -0,0 +1,28 @@
+import cvxpy as cvx
+
+from investos.portfolio.constraint_model.base_constraint import BaseConstraint
+from investos.util import remove_excluded_columns_pd, values_in_time
+
+
+class TradeReturnConstraint(BaseConstraint):
+    def __init__(self, forecast_returns, costs=[], limit=0.01, **kwargs):
+        self.costs = costs
+        self.limit = limit
+        super().__init__(**kwargs)
+        self.forecast_returns = remove_excluded_columns_pd(
+            forecast_returns, self.exclude_assets
+        )
+        self.forecast_returns -= self.limit
+
+    def _weight_expr(self, t, w_plus, z, v):
+        alpha_term = cvx.sum(
+            cvx.multiply(values_in_time(self.forecast_returns, t).values, z)
+        )
+
+        costs_li = []
+        for cost in self.costs:
+            # Trade specific, not portfolio level (hence 2 z's and no w_plus)
+            cost_expr, _ = cost.weight_expr(t, z, z, v, self.forecast_returns.index)
+            costs_li.append(cost_expr)
+
+        return (alpha_term - cvx.sum(costs_li)) >= 0
