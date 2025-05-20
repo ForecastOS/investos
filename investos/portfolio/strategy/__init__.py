991c5bfc218e48b62b2f598d20d1106a4e9a51f1
John Reese <j.charles.reese@gmail.com>
2025-05-20 11:53:36 -0700

    Adding dynamic VRA adjustment for risk model.

--- investos/portfolio/strategy/__init__.py
+++ investos/portfolio/strategy/__init__.py
@@ -2,3 +2,4 @@ from investos.portfolio.strategy.base_strategy import *
 from investos.portfolio.strategy.rank_long_short import *
 from investos.portfolio.strategy.spo import *
 from investos.portfolio.strategy.spo_tranches import *
+from investos.portfolio.strategy.spo_weights import *
